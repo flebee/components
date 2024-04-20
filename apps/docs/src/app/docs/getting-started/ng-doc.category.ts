fabb2da6a1df23972467f16de2924eb7a1eda703
Yeison Herrera <herrerayeison45@gmail.com>
2024-04-20 17:30:16 -0500

    docs(ui): add short documentation of available colors
    link #1

---
+++ apps/docs/src/app/docs/getting-started/ng-doc.category.ts
@@ -0,0 +1,8 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const GettingStartedCategory: NgDocCategory = {
+  title: 'Getting Started',
+  expandable: true
+};
+
+export default GettingStartedCategory;
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -2,8 +2,10 @@ import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
+import { flebeeUI } from '../../packages/ui/src';
+
 export default <Config>{
-  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
+  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html,md}'), ...createGlobPatternsForDependencies(__dirname)],
   theme: { extend: {} },
-  plugins: []
+  plugins: [flebeeUI()]
 };
