ed08df242482edfce1cb0e24db843269db7fe9eb
Yeison Herrera <herrerayeison45@gmail.com>
2024-04-20 21:04:02 -0500

    refactor(ui): expand border radius size of tailwind plugin
    link #1

--- packages/ui/src/generate-radius.ts
+++ packages/ui/src/generate-radius.ts
@@ -1,8 +1,8 @@
-export type Radius = Record<'lg' | 'md' | 'sm' | 'xs', `${number}${'px' | 'rem'}`>;
+export type Radius = Record<'lg' | 'md' | 'sm' | 'xl' | 'xs', `${number}${'px' | 'rem'}`>;
 
 export const prefix = 'bee' as const;
 
-const radius: Radius = { xs: '0.25rem', sm: '0.5rem', md: '1rem', lg: '1.5rem' } as const;
+const radius: Radius = { xs: '0.375rem', sm: '0.5rem', md: '0.875rem', lg: '1rem', xl: '1.25rem' } as const;
 
 export const generateRadius = (config: Partial<Radius> | undefined) => {
   const borderRadius = Object.entries({ ...radius, ...config }).map(([key, value]) => [`${prefix}-${key}`, value]);
