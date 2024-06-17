85dc995a784bc9b27fb7abd64881e5515a80f8c9
Yeison Herrera <herrerayeison45@gmail.com>
2024-06-16 19:54:24 -0500

    style(docs): add font Onest to documentation

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,11 +1,18 @@
 import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
-import { join } from 'path';
+import { join } from 'node:path';
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 import { flebeeUI } from '../../packages/ui/src';
 
 export default <Config>{
   content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html,md}'), ...createGlobPatternsForDependencies(__dirname)],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Onest Variable"', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   plugins: [flebeeUI()]
 };
