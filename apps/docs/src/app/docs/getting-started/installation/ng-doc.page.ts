2eead5376a715b3f707231e1974ea150e06ba3bd
Yeison Herrera <herrerayeison45@gmail.com>
2024-06-19 09:02:46 -0500

    docs: add installation documentation

---
+++ apps/docs/src/app/docs/getting-started/installation/ng-doc.page.ts
@@ -0,0 +1,12 @@
+import { NgDocPage } from '@ng-doc/core';
+
+import GettingStartedCategory from '../ng-doc.category';
+
+const InstallationPage: NgDocPage = {
+  order: 1,
+  mdFile: './index.md',
+  title: 'Installation',
+  category: GettingStartedCategory
+};
+
+export default InstallationPage;
