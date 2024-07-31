82890127d184898839e1e645b717c1f3b82deec8
Yeison Herrera <herrerayeison45@gmail.com>
2024-07-30 19:02:38 -0500

    feat(checkbox): add checkbox group component

--- packages/ui/checkbox/src/index.ts
+++ packages/ui/checkbox/src/index.ts
@@ -1,2 +1,3 @@
 export { BeeCheckbox } from './checkbox.component';
+export { BeeCheckboxGroup } from './checkbox-group.component';
 export type { BeeCheckboxSize } from './types';
--- packages/ui/checkbox/src/types.ts
+++ packages/ui/checkbox/src/types.ts
@@ -1,10 +1,17 @@
 import type { VariantProps } from 'cva';
 
-import type { base, icon, label, wrapper } from './style';
+import type { base, groupBase, groupDescription, groupLabel, groupWrapper, icon, label, wrapper } from './styles';
 
 type BeeCheckboxProps = VariantProps<typeof wrapper> &
   VariantProps<typeof label> &
   VariantProps<typeof icon> &
   VariantProps<typeof base>;
 
+type BeeCheckboxGroupProps = VariantProps<typeof groupWrapper> &
+  VariantProps<typeof groupBase> &
+  VariantProps<typeof groupLabel> &
+  VariantProps<typeof groupDescription>;
+
 export type BeeCheckboxSize = NonNullable<BeeCheckboxProps['size']>;
+
+export type BeeCheckboxGroupOrientation = NonNullable<BeeCheckboxGroupProps['orientation']>;
