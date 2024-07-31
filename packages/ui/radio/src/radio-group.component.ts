ef9314536b4705e94191029fa3ee5a472ac9f536
Yeison Herrera <herrerayeison45@gmail.com>
2024-07-30 19:06:14 -0500

    feat(radio): add support for error message with invalid status

--- packages/ui/radio/src/radio-group.component.ts
+++ packages/ui/radio/src/radio-group.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, type TemplateRef, booleanAttribute, computed, input, model } from '@angular/core';
+import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input, model, type TemplateRef } from '@angular/core';
 
 import { type BooleanInput, type Nullable, useId } from '@flebee/ui/core';
 import { BeeStringTemplate } from '@flebee/ui/string-template';
 
-import { groupBase, groupLabel } from './styles';
+import { groupBase, groupDescription, groupLabel } from './styles';
 import type { BeeRadioGroupOrientation, BeeRadioGroupSize } from './types';
 
 @Component({
@@ -15,7 +15,8 @@ import type { BeeRadioGroupOrientation, BeeRadioGroupSize } from './types';
     role: 'radiogroup',
     '[class]': 'baseClass()',
     '[attr.aria-labelledby]': 'id',
-    '[attr.aria-orientation]': 'orientation()'
+    '[attr.aria-orientation]': 'orientation()',
+    '[attr.aria-describedby]': 'describedById()'
   },
   template: `
     @if (label(); as label) {
@@ -23,18 +24,28 @@ import type { BeeRadioGroupOrientation, BeeRadioGroupSize } from './types';
     }
 
     <ng-content select="bee-radio" />
+
+    @if (hit(); as hit) {
+      <bee-string-template [id]="describedById()" [content]="hit" [class]="descriptionClass()" />
+    }
   `
 })
 export class BeeRadioGroup {
   public disabled = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
+  public invalid = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
   public orientation = input<BeeRadioGroupOrientation>('vertical');
-  public label = input<TemplateRef<void> | string>();
+  public errorMessage = input<string | TemplateRef<void>>();
+  public description = input<string | TemplateRef<void>>();
+  public label = input<string | TemplateRef<void>>();
   public size = input<BeeRadioGroupSize>('md');
   public value = model<Nullable<string>>();
   public class = input<string>();
   public name = input<string>();
 
   public baseClass = computed(() => groupBase({ orientation: this.orientation(), size: this.size(), class: this.class() }));
-  public labelClass = computed(() => groupLabel({ size: this.size() }));
-  public id = useId('bee-button-group');
+  public hit = computed(() => (!!this.errorMessage() && this.invalid() ? this.errorMessage() : this.description()));
+  public descriptionClass = computed(() => groupDescription({ size: this.size(), invalid: this.invalid() }));
+  public labelClass = computed(() => groupLabel({ size: this.size(), invalid: this.invalid() }));
+  public describedById = computed(() => (this.hit() ? `${this.id}-described` : undefined));
+  public id = useId('bee-radio-group');
 }
