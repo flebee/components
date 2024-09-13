ba26501e792702ee7d26e2f6e02cd4c4c0fc8407
Yeison Herrera <herrerayeison45@gmail.com>
2024-09-13 10:56:14 -0500

    feat(forms/template): add support for rendering templates, components, string, and HTML

---
+++ packages/forms/template/README.md
@@ -0,0 +1,3 @@
+# @flebee/forms/template
+
+Secondary entry point of `@flebee/forms`. It can be used by importing from `@flebee/forms/template`.
---
+++ packages/forms/template/src/with-template.ts
@@ -0,0 +1,40 @@
+import { computed, type InputSignalWithTransform, isSignal, type Signal, Type } from '@angular/core';
+
+import type {
+  BeeFieldConfig,
+  BeeFieldConfigValidWithoutControlKeys,
+  BeeFieldSignalProps,
+  BeeFieldTemplateValue,
+  OmitStrict,
+  PickStrict
+} from '@flebee/forms/core';
+
+export type UnwrapSignalInputs<Type> = {
+  [Prop in keyof Type as Type[Prop] extends InputSignalWithTransform<any, any>
+    ? Prop
+    : never]: Type[Prop] extends InputSignalWithTransform<any, infer WriteT> ? WriteT : never;
+};
+
+type IsEmptyObject<T> = keyof T extends never ? true : false;
+
+type InferComponentInputs<T> =
+  T extends Type<infer C> ? (IsEmptyObject<UnwrapSignalInputs<C>> extends true ? never : Partial<UnwrapSignalInputs<C>>) : never;
+
+type BaseFieldConfig = OmitStrict<PickStrict<BeeFieldConfig, BeeFieldConfigValidWithoutControlKeys>, 'props'>;
+
+type BeeFieldConfigTemplate<Template extends BeeFieldTemplateValue> = BaseFieldConfig &
+  (Template extends Type<any>
+    ? { template: Template; inputs?: BeeFieldSignalProps<InferComponentInputs<Template>> }
+    : { template: Signal<Template> | Template; inputs?: undefined });
+
+export function withTemplate<Template extends BeeFieldTemplateValue>(config: BeeFieldConfigTemplate<Template>) {
+  const { inputs, ...options } = config;
+
+  const computedInputs = computed(() => {
+    const safeInputs = Object.entries(inputs || {}).map(([key, value]) => [key, isSignal(value) ? value() : value]);
+
+    return Object.fromEntries(safeInputs);
+  });
+
+  return { ...options, props: { inputs: computedInputs as InferComponentInputs<Template> } };
+}
