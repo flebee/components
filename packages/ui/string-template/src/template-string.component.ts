731ff129019b3951e0135f3ba46a5e510ca3c932
Yeison Herrera <herrerayeison45@gmail.com>
2024-09-13 10:06:48 -0500

    refactor(string-template): simplify for cleaner implementation

--- packages/ui/string-template/src/template-string.component.ts
+++ packages/ui/string-template/src/template-string.component.ts
@@ -1,80 +1,30 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  effect,
-  type EmbeddedViewRef,
-  input,
-  type TemplateRef,
-  untracked,
-  viewChild,
-  ViewContainerRef,
-  ViewEncapsulation
-} from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, type TemplateRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
   standalone: true,
+  imports: [NgTemplateOutlet],
   selector: 'bee-string-template',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <ng-container #viewRef />
+    {{ string() }}
 
-    <ng-template #contentTpl> {{ content() }} </ng-template>
+    <ng-container *ngTemplateOutlet="templateRef(); context: context()" />
   `
 })
 export class BeeStringTemplate<Type> {
-  private _viewRef = viewChild.required('viewRef', { read: ViewContainerRef });
-  private _contentTpl = viewChild.required<TemplateRef<Type>>('contentTpl');
-  private _embeddedRef: EmbeddedViewRef<Type | void> | null = null;
-
   public content = input.required<string | TemplateRef<Type>>();
   public context = input<Type | undefined>(undefined);
 
-  constructor() {
-    effect(() => {
-      const contentTpl = untracked(this._contentTpl);
-      const viewRef = untracked(this._viewRef);
-      const content = this.content();
+  public string = computed(() => {
+    const content = this.content();
 
-      viewRef.clear();
+    return typeof content === 'string' ? content : '';
+  });
+  public templateRef = computed(() => {
+    const content = this.content();
 
-      this._embeddedRef?.destroy();
-      this._embeddedRef = null;
-
-      untracked(() => {
-        const safeContent = typeof content === 'string' ? contentTpl : content;
-        const context = this._createContextForwardProxy();
-
-        this._embeddedRef = viewRef.createEmbeddedView(safeContent, context);
-        this._embeddedRef.markForCheck();
-      });
-    });
-  }
-
-  /**
-   * For a given outlet instance, we create a proxy object that delegates
-   * to the user-specified context. This allows changing, or swapping out
-   * the context object completely without having to destroy/re-create the view.
-   */
-  private _createContextForwardProxy(): Type {
-    return <Type>new Proxy(
-      {},
-      {
-        set: (_target, prop, newValue) => {
-          const context = untracked(this.context);
-
-          if (!context) return false;
-
-          return Reflect.set(context, prop, newValue);
-        },
-        get: (_target, prop, receiver) => {
-          const context = untracked(this.context);
-
-          if (!context) return undefined;
-
-          return Reflect.get(context, prop, receiver);
-        }
-      }
-    );
-  }
+    return typeof content === 'string' ? null : content;
+  });
 }
