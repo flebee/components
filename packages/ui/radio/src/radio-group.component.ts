0819e8a4a5268f2e197f7073a801fb49c897c629
Yeison Herrera <herrerayeison45@gmail.com>
2024-09-23 20:17:14 -0500

    feat(ui/radio): support Angular Forms with control-value-accessor

--- packages/ui/radio/src/radio-group.component.ts
+++ packages/ui/radio/src/radio-group.component.ts
@@ -1,6 +1,7 @@
-import { booleanAttribute, ChangeDetectionStrategy, Component, computed, input, model, type TemplateRef } from '@angular/core';
+import { booleanAttribute, ChangeDetectionStrategy, Component, computed, inject, input, type TemplateRef } from '@angular/core';
 
 import { type BooleanInput, type Nullable, useId } from '@flebee/ui/core';
+import { BeeControlValueAccessor } from '@flebee/ui/core/control-value-accessor';
 import { BeeStringTemplate } from '@flebee/ui/string-template';
 
 import { groupBase, groupDescription, groupLabel } from './styles';
@@ -28,23 +29,23 @@ import type { BeeRadioGroupOrientation, BeeRadioGroupSize } from './types';
     @if (hit(); as hit) {
       <bee-string-template [id]="describedById()" [content]="hit" [class]="descriptionClass()" />
     }
-  `
+  `,
+  hostDirectives: [{ directive: BeeControlValueAccessor, inputs: ['value', 'disabled'], outputs: ['valueChange'] }]
 })
 export class BeeRadioGroup {
-  public disabled = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
   public invalid = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
   public orientation = input<BeeRadioGroupOrientation>('vertical');
   public errorMessage = input<string | TemplateRef<void>>();
   public description = input<string | TemplateRef<void>>();
   public label = input<string | TemplateRef<void>>();
   public size = input<BeeRadioGroupSize>('md');
-  public value = model<Nullable<string>>();
   public class = input<string>();
   public name = input<string>();
 
   public baseClass = computed(() => groupBase({ orientation: this.orientation(), size: this.size(), class: this.class() }));
   public hit = computed(() => (!!this.errorMessage() && this.invalid() ? this.errorMessage() : this.description()));
   public descriptionClass = computed(() => groupDescription({ size: this.size(), invalid: this.invalid() }));
+  public cva = inject<BeeControlValueAccessor<Nullable<number | string>>>(BeeControlValueAccessor);
   public labelClass = computed(() => groupLabel({ size: this.size(), invalid: this.invalid() }));
   public describedById = computed(() => (this.hit() ? `${this.id}-described` : undefined));
   public id = useId('bee-radio-group');
